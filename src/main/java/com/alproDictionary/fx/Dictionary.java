0e3686325901e07f9249596b97f15fa790f428b8
haniapn <81815215+haniapn@users.noreply.github.com>
2021-12-14 21:42:27 +0700

    sabar tambah kata

--- src/main/java/com/alproDictionary/fx/Dictionary.java
+++ src/main/java/com/alproDictionary/fx/Dictionary.java
@@ -1,10 +1,46 @@
 package com.alproDictionary.fx;
 
 public class Dictionary {
-    int dataSize = 100, current = 0;
+    int dataSize = 100,current = 0,
+    voidReturn = 0,arrLen = 0;
+
+    public int getArrLen() {
+        return arrLen;
+    }
+    public int getVoidReturn() {
+        return voidReturn;
+    }
+    public void setVoidReturn(int voidReturn) {
+        this.voidReturn = voidReturn;
+    }
     String[] englishArr = new String[dataSize];
     String[] indoArr = new String[dataSize];
     String[] indoDef = new String[dataSize];
+    void sort(){
+        boolean stop=false; String swap;
+        for (String arrCount:englishArr) {
+            if (arrCount!=null) arrLen++;
+        }
+        System.out.println("arrlen="+arrLen);
+        while (!stop){
+            stop=true;
+            for(int i=0; i<arrLen-1; i++){
+                System.out.println(englishArr[i]+".compareTo("+englishArr[i+1]+")");
+                if (englishArr[i].compareTo(englishArr[i+1])>0){
+                    swap=englishArr[i+1]; //SwapStr
+                    englishArr[i+1]=englishArr[i];
+                    englishArr[i]=swap;
+                    swap=indoArr[i+1]; //SwapStr
+                    indoArr[i+1]=indoArr[i];
+                    indoArr[i]=swap;
+                    swap=indoDef[i+1]; //SwapStr
+                    indoDef[i+1]=indoDef[i];
+                    indoDef[i]=swap;
+                    stop=false;
+                }
+            }// check
+        }
+    }
     Dictionary(){
         //init
 //      format:
@@ -13,15 +49,171 @@ public class Dictionary {
 //      int++
 
         englishArr[current]="LoremIpsum"; indoArr[current]="LoremIpsum";
-        indoDef[current]="nomina\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        indoDef[current]="nomina\n\n.";
         current++;
 
         englishArr[current]="Algorithm"; indoArr[current]="Algoritme";
-        indoDef[current]="nomina\nprosedur sistematis untuk memecahkan masalah matematis dalam langkah-langkah terbatas";
+        indoDef[current]="nomina\n\nprosedur sistematis untuk memecahkan masalah matematis dalam langkah-langkah terbatas.";
         current++;
 
         englishArr[current]="Argument"; indoArr[current]="Argumen";
-        indoDef[current]="nomina\nArgumen adalah cara untuk memberikan lebih banyak informasi ke suatu fungsi. Fungsi kemudian dapat menggunakan informasi itu saat berjalan, seperti variabel";
+        indoDef[current]="nomina\n\nArgumen adalah cara untuk memberikan lebih banyak informasi ke suatu fungsi. Fungsi kemudian dapat menggunakan informasi itu saat berjalan, seperti variabel.";
         current++;
+
+        //upi Start
+        englishArr[current]="Analysis"; indoArr[current]="Analisis";
+        indoDef[current]="nomina\n\npenyelidikan terhadap suatu peristiwa (karangan, perbuatan, dan sebagainya) untuk mengetahui keadaan yang sebenarnya (sebab-musabab, duduk perkaranya, dan sebagainya).";
+        current++;
+
+        englishArr[current]="Chat"; indoArr[current]="Obrolan";
+        indoDef[current]="nomina\n\npercakapan ringan dan santai; omong kosong:";
+        current++;
+
+        englishArr[current]="Connection"; indoArr[current]="Sambungan";
+        indoDef[current]="nomina\n\nkomunikasi yang terjadi apabila gagasan dan perasaan yang disampaikan pembawa pesan dapat menggugah dan menggerakkan hati penerima pesan; kontak antara pihak yang satu dan pihak yang lain; kontak batin.";
+        current++;
+
+        englishArr[current]="Copy"; indoArr[current]="Salin";
+        indoDef[current]="nomina\n\nmengutip (tulisan); menulis kembali; meniru.";
+        current++;
+
+        englishArr[current]="Communicate"; indoArr[current]="\tBerkomunikasi/Menyampaikan";
+        indoDef[current]="nomina\n\npengiriman dan penerimaan pesan atau berita antara dua orang atau lebih sehingga pesan yang dimaksud dapat dipahami; hubungan; kontak; 2 perhubungan.";
+        current++;
+
+        englishArr[current]="Computer"; indoArr[current]="Komputer";
+        indoDef[current]="nomina\n\nalat elektronik otomatis yang dapat menghitung atau mengolah data secara cermat menurut yang diinstruksikan, dan memberikan hasil pengolahan, serta dapat " +
+                         "menjalankan sistem multimedia (film, musik, televisi, faksimile, dan sebagainya), biasanya terdiri atas unit pemasukan, unit pengeluaran, unit penyimpanan, serta unit pengontrolan.";
+        current++;
+
+        englishArr[current]="Creator"; indoArr[current]="Pembuat";
+        indoDef[current]="nomina\n\nsekumpulan orang yang merupakan kesatuan untuk mengerjakan sesuatu.";
+        current++;
+
+        englishArr[current]="Database"; indoArr[current]="Kumpulan data\n";
+        indoDef[current]="nomina\n\npenampung yang menyimpan data yang Anda upload ke Analytics.";
+        current++;
+
+        englishArr[current]="Device"; indoArr[current]="Piranti/alat/perangkat";
+        indoDef[current]="nomina\n\nbenda yang dipakai untuk mengerjakan sesuatu: perkakas; perabot(an).";
+        current++;
+
+        englishArr[current]="Interface"; indoArr[current]="Antarmuka";
+        indoDef[current]="nomina\n\nhubungan atau batasan umum antara dua unit atau alat.";
+        current++;
+
+        englishArr[current]="Maintanance"; indoArr[current]="Pemeliharaan";
+        indoDef[current]="nomina\n\nuang yang dikeluarkan untuk mengadakan (mendirikan, melakukan, dan sebagainya) sesuatu; ongkos; belanja; pengeluaran.";
+        current++;
+
+        englishArr[current]="Monitor"; indoArr[current]="Memantau";
+        indoDef[current]="nomina\n\nmengamati atau mengecek dengan cermat, terutama untuk tujuan khusus; mengawasi; memonitor.";
+        current++;
+
+        englishArr[current]="Network"; indoArr[current]="Jaringan";
+        indoDef[current]="nomina\n\nSekelompok komputer yang terhubung yang bisa saling berbagi sumber daya (seperti printer atau modem) dan data.";
+        current++;
+
+        englishArr[current]="Programming"; indoArr[current]="Pemrograman";
+        indoDef[current]="nomina\n\nrancangan mengenai asas serta usaha (dalam ketatanegaraan, perekonomian, dan sebagainya) yang akan dijalankan.";
+        current++;
+
+        englishArr[current]="Project"; indoArr[current]="Proyek";
+        indoDef[current]="nomina\n\nrencana pekerjaan dengan sasaran khusus (pengairan, pembangkit tenaga listrik, dan sebagainya) dan dengan saat penyelesaian yang tegas.";
+        current++;
+        //upi End
+
+        //mangat hania
+        englishArr[current]="Recurtion"; indoArr[current]="Pengulangan";
+        indoDef[current]="nomina\n\nsebuah perintah pada yang memerintahkan code untuk mengulang sebuah proses sampai proses tersebut dihentikan oleh kondisi semula.";
+        current++;
+
+        englishArr[current]="Sitemap"; indoArr[current]="Peta Situs";
+        indoDef[current]="nomina\n\nsebuah model atau kerangka konten  pada sebuah situs web yang dirancang untuk membantu penggunan dan mesin pencari menavigasi situs tersebut.";
+        current++;
+
+        englishArr[current]="User Interface"; indoArr[current]="Antarmuka Pengguna";
+        indoDef[current]="nomina\n\nsebuah bentuk tampilan grafis dibuat sedemikian rupa yang berhubungan langsung dengan pengguna dan berfungsi sebagai penghubung antara pengguna." +
+                         " dengan sistem operasi sehingga aplikasi atau program dapat beroperasi sebagaimana mestinya. ";
+        current++;
+
+        englishArr[current]="User Flow"; indoArr[current]="Alur Pengguna";
+        indoDef[current]="nomina\n\nalur yang diambil oleh pengguna dari awal memasuki aplikasi atau website hingga keluar dari aplikasi atau website tersebut.";
+        current++;
+
+        englishArr[current]="User Experience"; indoArr[current]="Pengalaman Pengguna";
+        indoDef[current]="nomina\n\nebuah proses meningkatkan kepuasan pengguna (pengguna aplikasi, pengunjung website) dalam meningkatkan kegunaan dan kesenangan yang " +
+                         "diberikan dalam interaksi antara pengguna dan produk.";
+        current++;
+
+        englishArr[current]="Wireframe"; indoArr[current]="Mockup/Rancangan";
+        indoDef[current]="nomina\n\nrancangan awal sebuah desain web atau protoype secara full dan detail yang dibuat secara manual menggunakan software editing gambar seperti Photoshop/Figma/Adobe XD.";
+        current++;
+
+        englishArr[current]="jQuery"; indoArr[current]="Kamus JavaScript";
+        indoDef[current]="nomina\n\nkumpulan fungsi-fungsi JavaScript yang memudahkan penulisan kode JavaScript";
+        current++;
+
+        englishArr[current]="Library"; indoArr[current]="Kumpulan fungsi";
+        indoDef[current]="nomina\n\nsebuah kumpulan fungsi yang dapat digunakan untuk memudahkan pembuatan sebuah aplikasi.";
+        current++;
+
+        englishArr[current]="Bug"; indoArr[current]="Kesalahan Kode";
+        indoDef[current]="nomina\n\nsebuah program cacat atau kesalahan  dalam kode,  sehingga menyebabkan tampilan, grafis, karakter, sistem dan lainnya membuat " +
+                         "sebuah website atau program tidak bekerja seperti seharusnya.";
+        current++;
+
+        englishArr[current]=""; indoArr[current]="";
+        indoDef[current]="nomina\n\n";
+        current++;
+
+        englishArr[current]=""; indoArr[current]="";
+        indoDef[current]="nomina\n\n";
+        current++;
+
+        englishArr[current]=""; indoArr[current]="";
+        indoDef[current]="nomina\n\n";
+        current++;
+
+        englishArr[current]=""; indoArr[current]="";
+        indoDef[current]="nomina\n\n";
+        current++;
+
+        englishArr[current]=""; indoArr[current]="";
+        indoDef[current]="nomina\n\n";
+        current++;
+
+        englishArr[current]=""; indoArr[current]="";
+        indoDef[current]="nomina\n\n";
+        current++;
+
+
+
+        sort();
+    }
+    public void search(String forThis,int awal,int akhir){
+        int tengah = (awal+akhir)/2;
+        if (forThis.equals(englishArr[tengah])) {
+            setVoidReturn(tengah);
+            return;
+        }
+        else if(awal==tengah||akhir==tengah){
+            if (forThis.equals(englishArr[tengah+1])) {
+                setVoidReturn(tengah+1);
+                return;
+            }
+            else if (forThis.equals(englishArr[tengah-1])) {
+                setVoidReturn(tengah-1);
+                return;
+            }
+            System.out.println("\nngga nemu broh");
+            setVoidReturn(-1);
+            return;
+        }
+        if (forThis.compareTo(englishArr[tengah])<0) {
+            awal = 0;
+            akhir = tengah-1;
+        }else if(forThis.compareTo(englishArr[tengah])>0) awal = tengah+1;
+        search(forThis,awal,akhir);
     }
 }
